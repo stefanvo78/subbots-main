87ef25b5fbb741503a3c5a91d362403a6a57d0d0
Toby Bradshaw <tobybrad@microsoft.com>
2017-03-09 15:34:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,209 @@
+'use strict';
+
+var request = require('request');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var _mainBot = null;
+
+// Called from a subbot, let the main know we're here
+function registerSubBot(url, luisAppId, subKey) {
+  var mainControl = "http://localhost:3978/api/control";
+  var options =  {
+    uri:mainControl,
+    method: 'POST',
+    json: {
+      type:'luis', 
+      endpoint:url, 
+      luisAppId:luisAppId, 
+      subKey:subKey
+    }
+  };
+  request(options, function(err, response, body) {
+  });
+}
+
+// Map of subbots to LUIS models (RL: would also include regex recognisers)
+var _subs = {};
+function control(req, res, next) {
+  var json = req.body;
+  // RL: body would be signed and encrypted
+  if (json.type === "luis") {
+    _subs[json.endpoint] = [json.luisAppId, json.subKey];
+  }
+  next();
+}
+
+function timeBot() {
+
+  // What time is it ?
+
+  var url = 'http://localhost:3980/api/messages';
+  registerSubBot(url, null, null);
+
+  var server = restify.createServer();
+  server.use(restify.bodyParser({ mapParams: false }));
+  server.listen(3980, function () {
+     console.log('%s listening to %s', server.name, server.url); 
+  });
+
+  var connector = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID,
+      appPassword: process.env.MICROSOFT_APP_PASSWORD
+  });
+
+  server.post('/api/messages', connector.listen());
+  
+  var bot = new builder.UniversalBot(connector);
+  bot.dialog('/', [
+    (session, args) => {
+      session.send('timeBot: The time is ' + new Date().toTimeString());
+    }
+  ]);
+}
+
+function dateBot() {
+
+  // What date is it ?
+
+  var url = 'http://localhost:3982/api/messages';
+  registerSubBot(url, null, null);
+
+  var server = restify.createServer();
+  server.use(restify.bodyParser({ mapParams: false }));
+  server.listen(3982, function () {
+     console.log('%s listening to %s', server.name, server.url); 
+  });
+
+  var connector = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID,
+      appPassword: process.env.MICROSOFT_APP_PASSWORD
+  });
+
+  server.post('/api/messages', connector.listen());
+  
+  var bot = new builder.UniversalBot(connector);
+  bot.dialog('/', [
+    (session, args) => {
+      session.send('dateBot: The date is ' + new Date().toDateString());
+    }
+  ]);
+}
+
+/* Real version
+function askLuis(appId, subKey, q, endpoint) {
+  var uri = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${appId}?subscription-key=${subKey}&verbose=true&q=${q}`;
+  return new Promise((resolve, reject) => {
+    var options = {
+      uri: uri,
+      method : 'GET'
+    };
+    request(options, (err, response, body) => {
+      resolve([endpoint, response]);
+    })
+  });
+}
+*/
+
+// Mock version
+function askLUIS(appId, subKey, q, endpoint) {
+  return new Promise((resolve, reject) => {
+    var result = { topScoringIntent: { intent : "None", score : 0.9 } };
+    if (q.indexOf('time') != -1) {
+      if (endpoint.indexOf("3980") != -1) {
+        result.topScoringIntent.intent = "whatTimeIsIt";
+        result.topScoringIntent.score = 0.89;
+      }
+    }
+    else if (q.indexOf('date') != -1) {
+      if (endpoint.indexOf("3982") != -1) {
+        result.topScoringIntent.intent = "whatDateIsIt";
+        result.topScoringIntent.score = 0.85;
+      }
+    }
+    resolve([endpoint, result]);  
+  });
+}
+
+function routeToSub(uri, req) {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: uri,
+      method : 'POST',
+      json : true,
+      body : req.body,
+      headers : req.headers
+    }, 
+    (err, response, body) => {
+      resolve(response);
+    })
+  });
+}
+
+function router(req, res, next) {
+
+  var tasks = [];
+  for (var k in _subs) {
+    tasks.push(askLUIS(_subs[k][0], _subs[k][1], req.body.text, k));
+  }
+
+  Promise.all(tasks)
+  .then((results) => {
+
+    // Filter out 'None'
+    var intents = results.filter((result) => { 
+      return (result[1].topScoringIntent.intent != 'None'); }
+    )
+
+    if (intents.length) {
+
+      // Find the top scoring intent
+      var topIntent = intents.reduce((prev, curr) => { 
+        return prev[1].topScoringIntent.score < curr[1].topScoringIntent.score ? prev : curr; 
+      });
+
+      // Route the request to the sub bot
+      routeToSub(topIntent[0], req)
+      .then((result) => {
+        // Pipe response back
+        result.pipe(res);
+        next();
+      });
+    }
+    else {
+      // Nothing to handle the utterance
+      res.end();
+      next();
+    }
+  });
+}
+
+function main() {
+
+  var server = restify.createServer();
+  server.use(restify.bodyParser({ mapParams: false }));
+  server.listen(process.env.port || process.env.PORT || 3978, function () {
+     console.log('%s listening to %s', server.name, server.url); 
+  });
+
+  var connector = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID,
+      appPassword: process.env.MICROSOFT_APP_PASSWORD
+  });
+
+  server.post('/api/messages', (req, res, next) => {
+      return router(req, res, next);
+  });
+  server.post('/api/control', control);
+
+  var bot = new builder.UniversalBot(connector);
+  bot.dialog('/', [
+  ]);
+  _mainBot = bot;
+}
+
+if (require.main === module) {
+  main();
+  timeBot();
+  dateBot();
+}
